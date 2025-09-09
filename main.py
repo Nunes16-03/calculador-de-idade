644955991a3081e4609ee0ffda5bf391c7ed0aef
Nunes16-03 <viniciusln2020@gmail.com>
2025-09-09 10:07:21 -0300

    primeiro commit

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-saldo = 1000
-print("Saldo inicial: {saldo}")
-while True:
-    print("\nEscolha uma opção:")
-    print("1. Depositar")
-    print("2. Sacar")
-    print("3. Ver saldo")
-    print("4. Sair")
-    opcao = input("Opção: ")
-    if opcao == "1":
-        valor = float(input("Valor a depositar: "))
-        saldo += valor
-        print(f"Depósito de {valor} realizado. Novo saldo: {saldo}")
-    elif opcao == "2":
-        valor = float(input("Valor a sacar: "))
-        if valor > saldo:
-            print("Saldo insuficiente.")
-        else:
-            saldo -= valor
-            print(f"Saque de {valor} realizado. Novo saldo: {saldo}")
-    elif opcao == "3":
-        print(f"Saldo atual: {saldo}")
+ano = 2025 
+nascimento = int(input("Digite o ano do seu nascimento: "))
+idade = 0
 
-    elif opcao == "4":
-        print("Saindo...")
-        break
-    else:
-        print("Opção inválida.")
-        
+while ano > nascimento:
+    idade += 1
+    nascimento += 1
+
+print(f"Sua idade em {ano} é {idade} anos.")
